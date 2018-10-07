4b9d51064a7fed0956d1a6d76f090d9bbae27034
Kshitij Gupta <kshitijgm@gmail.com>
2018-10-07 14:00:00 +0100

    vendor: Switch to Lawnchair [2/2]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    
    vendor: Introduce overlay for Lawnchair default conf
    
    Change-Id: Ie05eaea58ed341d0f4f2f5dcc177663afe401de1
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    
    fix
    
    fix
    
    fix

--- config/common.mk
+++ config/common.mk
@@ -141,6 +141,18 @@ endif
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
+# Lawnchair
+PRODUCT_COPY_FILES += \
+    vendor/xtended/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    vendor/xtended/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.theme=com.potato.overlay.lawnconf
+
+# Lawnchair Default Configuration
+PRODUCT_PACKAGES += \
+    LawnConf
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
